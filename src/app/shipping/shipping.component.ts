adfd5c75ac38c575f0326049dd41a0fa47cbdd04
BlackyTay <tay@outlook.my>
2020-02-03 15:48:17 +0800

    Set shippingCosts by cartService.getShippingPrices()

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -12,6 +12,7 @@ export class ShippingComponent implements OnInit {
   constructor(private cartService: CartService) { }
 
   ngOnInit() {
+    this.shippingCosts = this.cartService.getShippingPrices();
   }
 
 }
